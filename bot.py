8169d6e04a60150b4f70c878f7b0ae6596551194
KRISHAN KASHYAP <46775503+Im-krishnkshyap@users.noreply.github.com>
2025-09-17 14:13:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -110,9 +110,9 @@ def build_message(date_str, updates):
             elif g == "GHAZIYABAD":
                 lines.append(f"गाजियाबाद ==== {v}")
             elif g == "GALI":
-                lines.append(f"गली की बलि ==== {v}")
+                lines.append(f"गली     ==== {v}")
             elif g == "DISAWER":
-                lines.append(f"दिसावर/दिल्ली==== {v}")
+                lines.append(f"दिसावर   ==== {v}")
 
     lines.append("√√√√√√√√√√√√√√√√√")
     return "\n".join(lines)
